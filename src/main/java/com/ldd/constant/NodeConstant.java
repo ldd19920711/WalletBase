003c4b71da2b7b8f4b369936dd97c2e742cd9351
李东东 <847026930@qq.com>
2022-01-19 17:09:39 +0800

    测试修改1

--- src/main/java/com/ldd/constant/NodeConstant.java
+++ src/main/java/com/ldd/constant/NodeConstant.java
@@ -3,7 +3,7 @@ package com.ldd.constant;
 public class NodeConstant {
   //https://mainnet.infura.io/v3/ea23a1df0c16486ab0dd0f48a6566f4c
   //https://ropsten.infura.io/v3/ea23a1df0c16486ab0dd0f48a6566f4c
-
+  //1
 
 
 }
